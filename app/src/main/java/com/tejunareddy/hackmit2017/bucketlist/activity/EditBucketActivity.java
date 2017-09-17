d221649e00ac9352c7e3b5ad30a3b71ce5d3337d
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-09-17 04:32:52 -0400

    Saving of edits

--- app/src/main/java/com/tejunareddy/hackmit2017/bucketlist/activity/EditBucketActivity.java
+++ app/src/main/java/com/tejunareddy/hackmit2017/bucketlist/activity/EditBucketActivity.java
@@ -1,34 +1,76 @@
 package com.tejunareddy.hackmit2017.bucketlist.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.tejunareddy.hackmit2017.bucketlist.R;
 import com.tejunareddy.hackmit2017.bucketlist.model.BucketListItem;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class EditBucketActivity extends AppCompatActivity {
 
     public static BucketListItem bucket = null;
+    private FloatingActionButton fab;
+    private ImageView cityImageView;
+    private EditText dayView;
+    private EditText startDateView;
+    private EditText endDateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_bucket);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        toolbar.setTitle(bucket.getEndCity());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        cityImageView = (ImageView) findViewById(R.id.bucket_view_image);
+        cityImageView.setImageBitmap(bucket.getCityPicture());
+
+        dayView = (EditText) findViewById(R.id.bucket_edit_days);
+        dayView.setText(bucket.getDuration());
+
+        startDateView = (EditText) findViewById(R.id.bucket_edit_startDate);
+        startDateView.setText(dateToString(bucket.getStartDate()));
+
+        endDateView = (EditText) findViewById(R.id.bucket_edit_endDate);
+        endDateView.setText(dateToString(bucket.getEndDate()));
+
+        fab = (FloatingActionButton) findViewById(R.id.bucket_edit_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("M/d/YYYY", Locale.US);
+                try {
+                    Date startDate = simpleDateFormat.parse(startDateView.getText().toString());
+                    Date endDate = simpleDateFormat.parse(endDateView.getText().toString());
+
+                    bucket.setStartDate(startDate);
+                    bucket.setStartDate(endDate);
+
+                } catch (ParseException e) {
+                    return;
+                }
+
+                bucket.setDuration(Integer.parseInt(dayView.getText().toString()));
+
+                // TODO save item to api
+
+                finish();
             }
         });
     }
 
+    private String dateToString(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("M/d/YYYY", Locale.US);
+        return simpleDateFormat.format(date);
+    }
 }
