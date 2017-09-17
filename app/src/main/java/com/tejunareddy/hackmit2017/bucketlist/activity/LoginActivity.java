3ed71c234b944735b7237242b04fa8b4e3672edb
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-09-17 07:29:22 -0400

    Basic login http

--- app/src/main/java/com/tejunareddy/hackmit2017/bucketlist/activity/LoginActivity.java
+++ app/src/main/java/com/tejunareddy/hackmit2017/bucketlist/activity/LoginActivity.java
@@ -2,7 +2,6 @@ package com.tejunareddy.hackmit2017.bucketlist.activity;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,28 +14,28 @@ import android.widget.ProgressBar;
 
 import com.tejunareddy.hackmit2017.bucketlist.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "admin:password", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
 
     // UI references.
     private EditText mEmailView;
     private EditText mPasswordView;
     private ProgressBar mProgressView;
-    private View mLoginFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +54,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
         mProgressView = (ProgressBar) findViewById(R.id.login_progress);
     }
 
@@ -65,10 +63,6 @@ public class LoginActivity extends AppCompatActivity {
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
@@ -105,20 +99,50 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
+
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+
+            // FIXME example of the post request here
+            OkHttpClient client = new OkHttpClient();
+            RequestBody body = RequestBody.create(JSON, "{}");
+            Request request = new Request.Builder()
+                    .url("http://hackmit2017.pythonanywhere.com/login?email=" + email + "&password=" + password)
+                    .post(body)
+                    .build();
+
+            Response response = null;
+            try {
+                // Json parsing here
+                response = client.newCall(request).execute();
+                JSONObject jsonObject = new JSONObject(response.body().string());
+                boolean success = jsonObject.getBoolean("success");
+
+                showProgress(false);
+                if (success) {
+                    // Go to the next activity here
+                    // FIXME pass in user id
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    mPasswordView.setError("Incorrect login information");
+                    mPasswordView.requestFocus();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return true;
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return true;
     }
 
     /**
@@ -128,65 +152,6 @@ public class LoginActivity extends AppCompatActivity {
     private void showProgress(final boolean show) {
         mProgressView.setIndeterminate(true);
         mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-    }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
     }
 }
 
