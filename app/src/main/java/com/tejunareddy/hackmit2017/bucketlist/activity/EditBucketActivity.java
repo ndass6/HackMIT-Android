c0beebbc044023261ecee5dd647331a77847db9d
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-09-17 09:17:59 -0400

    put call on resource update

--- app/src/main/java/com/tejunareddy/hackmit2017/bucketlist/activity/EditBucketActivity.java
+++ app/src/main/java/com/tejunareddy/hackmit2017/bucketlist/activity/EditBucketActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -11,13 +12,25 @@ import android.widget.ImageView;
 import com.tejunareddy.hackmit2017.bucketlist.R;
 import com.tejunareddy.hackmit2017.bucketlist.model.BucketListItem;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 public class EditBucketActivity extends AppCompatActivity {
 
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
     public static BucketListItem bucket = null;
     private FloatingActionButton fab;
     private ImageView cityImageView;
@@ -62,7 +75,34 @@ public class EditBucketActivity extends AppCompatActivity {
 
                 bucket.setDuration(Integer.parseInt(dayView.getText().toString()));
 
+                SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("YYYY-MM-dd");
+
                 // TODO save item to api
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("http://hackmit2017.pythonanywhere.com/user/" + LoginActivity.userId
+                                + "/bucket-item/" + bucket.getEndCity()
+                                + "?start_date=" + simpleDateFormat1.format(bucket.getUserSetStartDate())
+                                + "&end_date=" + simpleDateFormat1.format(bucket.getUserSetStartDate())
+                                + "&duration=" + bucket.getDuration())
+                        .put(RequestBody.create(JSON, "{}"))
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        try (ResponseBody responseBody = response.body()) {
+                            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                            Log.d("res", responseBody.string());
+                        }
+                    }
+                });
 
                 finish();
             }
